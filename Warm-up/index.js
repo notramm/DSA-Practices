146244f72e3f9b6422daf2e704f348c7d1d7c397
notramm <muduliram09@gmail.com>
2025-12-26 15:51:54 +0530

    added another warm-up question

--- Warm-up/index.js
+++ Warm-up/index.js
@@ -1,45 +1,60 @@
 // Searching an element in an array and return its index
-function searchElement(arr, target){
-  for (let i = 0; i < arr.length; i++){
-    if (arr[i] == target){
-      return i;
-    }
-  }
-  return -1;
-}
-arr = [1, 6, 8, 9, 3, 4, 5, 7, 2, 0];
-console.log(searchElement(arr, 4));
+// function searchElement(arr, target){
+//   for (let i = 0; i < arr.length; i++){
+//     if (arr[i] == target){
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+// arr = [1, 6, 8, 9, 3, 4, 5, 7, 2, 0];
+// console.log(searchElement(arr, 4));
 
 // Count negative numbers in an array
-let arr = [ -1, 8, 5, -2, 9, -3, 0, 7, 6, -4, 2, 1, 4, 3 ];
+// let arr = [ -1, 8, 5, -2, 9, -3, 0, 7, 6, -4, 2, 1, 4, 3 ];
 
-let count = 0;
-function segregateNegative(arr){
-  for (let i = 0; i < arr.length; i++ ){
-    if(arr[i]< 0){
-      count++;
-      console.log(`${arr[i]} and index is ${i}` );
-    }
-  }
-  return count;
-}
+// let count = 0;
+// function segregateNegative(arr){
+//   for (let i = 0; i < arr.length; i++ ){
+//     if(arr[i]< 0){
+//       count++;
+//       console.log(`${arr[i]} and index is ${i}` );
+//     }
+//   }
+//   return count;
+// }
 
-console.log(segregateNegative(arr));
+// console.log(segregateNegative(arr));
 
 
 // Find the largest element in an array
-let  arr = [1, 2, 3, 4, 5, 10, 6, 9, 20, 12];
+// let  arr = [1, 2, 3, 4, 5, 10, 6, 9, 20, 12];
 
-let largest = -1;
+// let largest = arr[0];
 
-function LargestElement(arr){
+// function LargestElement(arr){
 
+//   for (let i = 0; i< arr.length; i++){
+//     if (arr[i] > largest){
+//       largest = arr[i];
+//     }
+//   }
+//   return largest; 
+// }
+
+// console.log(LargestElement(arr));
+
+// Find the smallest element in an array
+let  arr = [1, 2, 3, 4, 5, 10, 6, 9, 0, 20, 12];
+
+let smallest = arr[0];
+
+function SmallestElement(arr){
   for (let i = 0; i< arr.length; i++){
-    if (arr[i] > largest){
-      largest = arr[i];
+    if (arr[i]< smallest){
+      smallest = arr[i];
     }
   }
-  return largest; 
+  return smallest;
 }
-
-console.log(LargestElement(arr));
+ console.log(SmallestElement(arr));
