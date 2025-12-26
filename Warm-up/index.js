63a1a9d09da657b099e2995e3ad1f7f42615ae18
notramm <muduliram09@gmail.com>
2025-12-26 15:42:59 +0530

    Edit Warmups

--- Warm-up/index.js
+++ Warm-up/index.js
@@ -1,30 +1,30 @@
 // Searching an element in an array and return its index
-// function searchElement(arr, target){
-//   for (let i = 0; i < arr.length; i++){
-//     if (arr[i] == target){
-//       return i;
-//     }
-//   }
-//   return -1;
-// }
-// arr = [1, 6, 8, 9, 3, 4, 5, 7, 2, 0];
-// console.log(searchElement(arr, 4));
+function searchElement(arr, target){
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] == target){
+      return i;
+    }
+  }
+  return -1;
+}
+arr = [1, 6, 8, 9, 3, 4, 5, 7, 2, 0];
+console.log(searchElement(arr, 4));
 
 // Count negative numbers in an array
-// let arr = [ -1, 8, 5, -2, 9, -3, 0, 7, 6, -4, 2, 1, 4, 3 ];
+let arr = [ -1, 8, 5, -2, 9, -3, 0, 7, 6, -4, 2, 1, 4, 3 ];
 
-// let count = 0;
-// function segregateNegative(arr){
-//   for (let i = 0; i < arr.length; i++ ){
-//     if(arr[i]< 0){
-//       count++;
-//       console.log(`${arr[i]} and index is ${i}` );
-//     }
-//   }
-//   return count;
-// }
+let count = 0;
+function segregateNegative(arr){
+  for (let i = 0; i < arr.length; i++ ){
+    if(arr[i]< 0){
+      count++;
+      console.log(`${arr[i]} and index is ${i}` );
+    }
+  }
+  return count;
+}
 
-// console.log(segregateNegative(arr));
+console.log(segregateNegative(arr));
 
 
 // Find the largest element in an array
