f1395e1dae9ff3ba2568940434ee63003e6ff947
notramm <muduliram09@gmail.com>
2026-01-02 23:15:48 +0530

    Refactor SecoundLargest function to improve readability and remove unnecessary code

--- secondLargestnum.js
+++ secondLargestnum.js
@@ -1,37 +1,20 @@
-let  arr = [1, 2, 3, 14, 5, 7, 6, 9, 20, 12];
-
-let firstlargest = arr[0];
-let secondlargest = arr[0];
+let  arr = [1, 2, 3, 14, 5, 7, 6, 9, 12];
 
 function SecoundLargest(arr){
 
+  let firstlargest = arr[0];
+  let secondlargest = arr[0];
+
   for (let i = 0; i< arr.length; i++){
     if (arr[i] > firstlargest){
+        secondlargest = firstlargest;
         firstlargest = arr[i];
-        for (let j = 0; j < i; j++){
-            if (secondlargest < arr[j]){
-                secondlargest = arr[j];
-            }
-        }
+    }
+    else if(arr[i] > secondlargest && arr[i] != firstlargest){
+        secondlargest = arr[i];
     }
   }
   return secondlargest; 
 }
 
 console.log(SecoundLargest(arr));
-
-// function SecoundLargest(arr){
-
-//   for (let i = 0; i< arr.length; i++){
-//     if (arr[i] > firstlargest){
-//         secondlargest = firstlargest;
-//         firstlargest = arr[i];
-//     }
-//     else if(arr[i] > secondlargest){
-//         secondlargest = arr[i];
-//     }
-//   }
-//   return secondlargest; 
-// }
-
-// console.log(SecoundLargest(arr));
